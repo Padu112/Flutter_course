f020e25d434a73b94bfa6a784b64ec5ca54f7178
andrei <andrei.pdu@gmail.com>
2019-05-27 02:33:58 +0100

    added app drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/pages/products.dart';
 // import 'package:flutter/rendering.dart';
 
-import './pages/home.dart';
+import './pages/auth.dart';
 
 main() {
   // debugPaintSizeEnabled = true;
@@ -19,6 +20,6 @@ class MyApp extends StatelessWidget {
             brightness: Brightness.light,
             primarySwatch: Colors.deepOrange,
             accentColor: Colors.deepPurple),
-        home: HomePage());
+        home: AuthPage());
   }
 }
---
+++ lib/pages/products_admin.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+
+class ProductsAdminPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('All Products'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => ProductsPage()),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('EasyList'),
+      ),
+      body: Center(
+        child: Text('TODO'),
+      ),
+    );
+  }
+}
